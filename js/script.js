aa207082b3d4a35b96a1258a0adcf1d3bc89f9df
Petr <zhirkovpetr95@gmail.com>
2023-09-09 22:02:39 +0300

    feat: add function showTime() (shows the current time)

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
 const time = document.querySelector('.time');
+
+function showTime() {
+  const date = new Date();
+  time.textContent = date.toLocaleTimeString();
+  setTimeout(showTime, 1000);
+}
+showTime();
