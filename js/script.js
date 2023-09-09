98a61bd3d00b2c2092b25d77bfb02dd200a823ca
Petr <zhirkovpetr95@gmail.com>
2023-09-09 22:10:34 +0300

    feat: add function showDate() (shows the current date)

--- js/script.js
+++ js/script.js
@@ -1,8 +1,17 @@
 const time = document.querySelector('.time');
+const date = document.querySelector('.date');
 
 function showTime() {
-  const date = new Date();
-  time.textContent = date.toLocaleTimeString();
+  const dateTime = new Date();
+  time.textContent = dateTime.toLocaleTimeString();
+  showDate()
   setTimeout(showTime, 1000);
 }
+
 showTime();
+
+function showDate() {
+  let dateDate = new Date();
+  const options = {weekday: 'long', day: 'numeric', month: 'long'};
+  date.textContent = dateDate.toLocaleDateString('en-US', options);
+}
