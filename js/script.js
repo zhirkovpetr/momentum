34445a47a7c324ff26b09c3c11df6b73390ce806
Petr <zhirkovpetr95@gmail.com>
2023-09-11 23:33:24 +0300

    feat: add getting weather for a specific city

--- js/script.js
+++ js/script.js
@@ -6,6 +6,10 @@ window.addEventListener("DOMContentLoaded", () => {
   const body = document.querySelector('body');
   const slideNext = document.querySelector('.slide-next');
   const slidePrev = document.querySelector('.slide-prev');
+  const weatherIcon = document.querySelector('.weather-icon');
+  const temperature = document.querySelector('.temperature');
+  const weatherDescription = document.querySelector('.weather-description');
+  const city = document.querySelector('.city');
   let randomNum = getRandomNum();
 
   getName();
@@ -90,6 +94,30 @@ window.addEventListener("DOMContentLoaded", () => {
     randomNum = randomNum > -1 ? 20 : randomNum - 1
     setBg()
   }
+
+  /*Погода*/
+  async function getWeather() {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.textContent}&lang=ru&appid=e612e6250fdb37b14c30611dad255a26&units=metric`;
+    const res = await fetch(url);
+    const data = await res.json();
+
+    weatherIcon.className = 'weather-icon owf';
+    weatherIcon.classList.add(`owf-${data.weather[0].id}`);
+    temperature.textContent = `${data.main.temp.toFixed(0)}°C`;
+    weatherDescription.textContent = data.weather[0].description;
+  }
+
+  function setCity(event) {
+    if (event.code === 'Enter') {
+      getWeather();
+      city.blur();
+    }
+  }
+
+  getWeather()
+  city.addEventListener('keyup', setCity);
+  city.addEventListener("blur", setCity);
+
 
   /*Плавная смена фоновых изображений*/
   function getRandomNum(min, max) {
